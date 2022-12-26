ed6e7f0602b14d175e37a9220ff9a243750518d0
talsvorai1 <121238453+talsvorai1@users.noreply.github.com>
2022-12-26 11:47:59 +0200

    Update main_APIgateway.tf

--- Final_Task2/api_gateway/main_APIgateway.tf
+++ Final_Task2/api_gateway/main_APIgateway.tf
@@ -7,42 +7,39 @@ provider "aws" {
 data "aws_lambda_function" "function" {
   function_name = "my-function"
 }
-#Importing IAM role
-data "aws_iam_role" "hello_lambda_exec" {
-  name = "hello-lambda"
-}
+
 #Creating gateway
-resource "aws_api_gateway_rest_api" "my-api" {
-  name        = "my-api"
-  description = "My API"
+resource "aws_api_gateway_rest_api" "new-api" {
+  name        = "new-api"
+  description = "My new API"
 }
 
-resource "aws_api_gateway_resource" "my-api" {
-  rest_api_id = aws_api_gateway_rest_api.my-api.id
-  parent_id   = aws_api_gateway_rest_api.my-api.root_resource_id
-  path_part   = "my-api"
+resource "aws_api_gateway_resource" "new-api" {
+  rest_api_id = aws_api_gateway_rest_api.new-api.id
+  parent_id   = aws_api_gateway_rest_api.new-api.root_resource_id
+  path_part   = "new-api"
 }
 
 #Creating GET method
-resource "aws_api_gateway_method" "my-api" {
-  rest_api_id   = aws_api_gateway_rest_api.my-api.id
-  resource_id   = aws_api_gateway_resource.my-api.id
-  http_method   = "GET"
+resource "aws_api_gateway_method" "new-api" {
+  rest_api_id   = aws_api_gateway_rest_api.new-api.id
+  resource_id   = aws_api_gateway_resource.new-api.id
+  http_method   = "POST"
   authorization = "NONE"
   api_key_required = false
 }
 #Integrating method with Lambda function
-resource "aws_api_gateway_integration" "my-api" {
-  depends_on    = [aws_api_gateway_method.my-api]
-  rest_api_id = aws_api_gateway_rest_api.my-api.id
-  resource_id = aws_api_gateway_resource.my-api.id
-  http_method = aws_api_gateway_method.my-api.http_method
+resource "aws_api_gateway_integration" "new-api" {
+  depends_on    = [aws_api_gateway_method.new-api]
+  rest_api_id = aws_api_gateway_rest_api.new-api.id
+  resource_id = aws_api_gateway_resource.new-api.id
+  http_method = aws_api_gateway_method.new-api.http_method
   type        = "AWS_PROXY"
   uri         = data.aws_lambda_function.function.invoke_arn
   integration_http_method = "POST"
 }
 #Deploying - To access url it is in api gateway stages: prod
-resource "aws_api_gateway_deployment" "my-api" {
-  rest_api_id = aws_api_gateway_rest_api.my-api.id
+resource "aws_api_gateway_deployment" "new-api" {
+  rest_api_id = aws_api_gateway_rest_api.new-api.id
   stage_name  = "prod"
 }
